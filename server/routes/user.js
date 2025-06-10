79ef2185d7bd0f505f182c22023f8e86ac2afe51
cs-Deepak <91629162dky@gmail.com>
2025-06-10 11:56:06 +0530

    Update user.js

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,7 @@ const User = require("./models/User");
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://todo-ugwc.vercel.app", credentials: true }));
 app.use(express.json());
 
 // Connect MongoDB
